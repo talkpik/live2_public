af379605803d5e2f8a08042fc0109baf934168c6
J <J@afshans-MacBook-Pro.local>
2020-10-11 22:41:32 -0700

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from './Navbar';
+import Header from './Header';
+import Services from './Services';
+import Seeker from './Seeker';
+import Speaker from './Speaker';
+import Events from './Events';
+import Team from './Team';
+import Contact from './Contact';
+import Footer from './Footer';
+function App() {
+  return (
+	<BrowserRouter>
+    <div className="App">
+	<Navbar/>
+	<div>
+        				<Switch>
+          					<Route exact path="/"><Header /></Route>
+          					<Route path="/services"><Services /></Route>
+							<Route path="/seeker"><Seeker /></Route>
+							<Route path="/speaker"><Speaker /></Route>
+							<Route path="/events"><Events /></Route>
+							<Route path="/team"><Team /></Route>
+							<Route path="/contact"><Contact /></Route>
+        				</Switch>
+      				</div>
+				<Footer />
+    </div>
+	</BrowserRouter>
+  );
+}
+
+export default App;
