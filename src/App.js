413e12addaea1dad9dc0f87100b50640100c0aba
J <J@afshans-MacBook-Pro.local>
2020-10-14 00:39:12 -0700

    Navigation Menu changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import NavigationMenu from './NavigationMenu';
+import NavigationMenu from './extra';
 import Header from './Header';
 import Services from './Services';
 import Seeker from './Seeker';
@@ -14,17 +14,6 @@ function App() {
 	<BrowserRouter>
     <div className="App">
 	<NavigationMenu/>
-	<div>
-        				<Switch>
-          					<Route exact path="/"><Header /></Route>
-          					<Route path="/services"><Services /></Route>
-							<Route path="/seeker"><Seeker /></Route>
-							<Route path="/speaker"><Speaker /></Route>
-							<Route path="/events"><Events /></Route>
-							<Route path="/team"><Team /></Route>
-							<Route path="/contact"><Contact /></Route>
-        				</Switch>
-      				</div>
 				<Footer />
     </div>
 	</BrowserRouter>
